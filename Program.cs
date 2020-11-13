3e89ad1b644eabbcd9061619bcef05bef2d10962
kosaka-bun <admin@honoka.de>
2020-11-13 22:40:56 +0800

    v1.0

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBeMadeStatic.Global
+// ReSharper disable StringLiteralTypo
+
+namespace JarLauncher {
+
+internal static class Program {
+    
+    private static void Main(string[] args) {
+        if (args.Length < 1) {
+            MessageBox.Show("没有提供要打开的文件", "JarLauncher",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+        var launcher = new Launcher();
+        launcher.checkVmOptionsFile();
+        string vmOptions = launcher.readVmOptions();
+        //启动命令行
+        var process = new Process {
+            StartInfo = {
+                //设置要启动的应用程序
+                FileName = "cmd.exe",
+                //是否使用操作系统shell启动
+                UseShellExecute = false,
+                //接受来自调用程序的输入信息
+                RedirectStandardInput = true,
+                //输出信息
+                RedirectStandardOutput = true,
+                //输出错误
+                RedirectStandardError = true,
+                //不显示程序窗口
+                CreateNoWindow = true
+            }
+        };
+        //启动程序
+        process.Start();
+        process.StandardInput.WriteLine($"javaw {vmOptions} -jar \"{args[0]}\"");
+        process.StandardInput.WriteLine("exit");   //需要有这句，不然程序会挂机
+        //等待程序执行完退出进程
+        process.WaitForExit();
+        process.Close();
+    }
+}
+
+internal class Launcher {
+
+    public string readVmOptions() {
+        var fileStream = new FileStream(vmOptionsPath, FileMode.Open,
+            FileAccess.Read);
+        var streamReader = new StreamReader(fileStream);
+        string content = streamReader.ReadToEnd();
+        streamReader.Close();
+        fileStream.Close();
+        return content;
+    }
+
+    public void checkVmOptionsFile() {
+        if(File.Exists(vmOptionsPath)) return;
+        var fileStream = new FileStream(vmOptionsPath, FileMode.Create,
+            FileAccess.ReadWrite);
+        var streamWriter = new StreamWriter(fileStream);
+        streamWriter.Write(defaultVmOptions);
+        streamWriter.Close();
+        fileStream.Close();
+    }
+
+    private static readonly string
+        vmOptionsPath = Application.StartupPath + "\\vm_options.txt";
+    private const string defaultVmOptions = "-Dfile.encoding=UTF-8";
+}
+
+}
